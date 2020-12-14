68550db1dcbde59899b3dd4544daa17a9de0b7ee
Lex_Agone <enzo.vetromile@gmail.com>
2020-12-14 14:19:37 +0100

    Header html
    Que l'html

---
+++ Lylart/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <!-- Encodage des caractères -->
+    <meta charset="UTF-8">
+    <!-- Viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Nom des auteurs -->
+    <meta name="author" content="Enzo Vetromile, Axel Avinin">
+    <!-- Description pour le référencement -->
+    <meta name="description" content="Lylart est un site de génération et de partage de ces derniers.">
+    <!-- Icone de l'onglet -->
+    <link rel="icon" type="image/x-icon" href="">
+    <!-- Titre de l'onglet -->
+    <title>Lylart Gallery</title>
+    <!-- Importer CSS et scripts -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <a href="">
+            <div>
+                <p>Générateur</p>
+            </div>
+        </a>
+        <a href="">
+            <div>
+                <p>Lylart</p> <!-- à remplacer avec le logo ? -->
+            </div>
+        </a>
+        <a href="">
+            <div>
+                <p>Utilisateur</p>
+            </div>
+        </a>
+    </header>
