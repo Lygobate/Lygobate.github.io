753a7dcbf262478db3df63dadbe7c17c088f4f24
Lygobate <63309083+Lygobate@users.noreply.github.com>
2020-12-14 15:37:32 +0100

    bug

---
+++ Lylart/user.php
@@ -0,0 +1,19 @@
+<?php
+    require_once "header.php";
+?>
+
+    <main>
+      <div class="grid">
+        <div class="pdp-box">
+          <img src="" alt="">
+        </div>
+        <div>
+          <p>Changer de photo de profil</p>
+        </div>
+        <div class="infos-box">
+
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
