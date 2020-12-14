f20e168e0c3bd72e6d17d5dd0e0cfebaf1f4f30c
Lex_Agone <enzo.vetromile@gmail.com>
2020-12-14 20:58:47 +0100

    continue

--- Lylart/index.php
+++ Lylart/index.php
@@ -6,13 +6,13 @@
     <div class="video">
         <div>
             <div>
-                <h1>Think.</h1>
-                <h1>Create.</h1>
-                <h1>Share.</h1>
+                <h1>Slogan</h1>
+                <h2>Autre chose ?</h2>
+                <h2>Peut-être...</h2>
             </div>
             <a href="gen/"><div class="button">Créer</div></a>
         </div>
-        <video loop controls playsinline > <!-- autoplay loop controls playsinline-->
+        <video loop playsinline > <!-- autoplay loop controls playsinline-->
             <source src="video/EEEAAAOOO.mp4" type="video/mp4">
         </video>
     </div>
