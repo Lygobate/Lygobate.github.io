0fc295e410defd569e0216b64458385872cdcb22
Lex_Agone <enzo.vetromile@gmail.com>
2020-12-14 15:39:11 +0100

    Avancement accueil

--- Lylart/index.php
+++ Lylart/index.php
@@ -2,5 +2,11 @@
     require_once "header.php";
 ?>
 
+<div class="video">
+    <div style="position: fixed; z-index: -99; width: 100%; height: 100%">
+        <iframe frameborder="0" height="auto" width="100%" src="https://youtube.com/embed/L_jWHffIx5E?autoplay=1&controls=0&showinfo=0&autohide=1"></iframe>
+    </div>
+</div>
+
 </body>
 </html>
