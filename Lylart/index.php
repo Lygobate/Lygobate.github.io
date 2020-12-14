eb7571e1de8486bd8607513ba26e103da21061cd
Lex_Agone <enzo.vetromile@gmail.com>
2020-12-14 16:43:53 +0100

    video

--- Lylart/index.php
+++ Lylart/index.php
@@ -2,9 +2,15 @@
     require_once "header.php";
 ?>
 
-<div class="video">
-    <div style="position: fixed; z-index: -99; width: 100%; height: 100%">
-        <iframe frameborder="0" height="auto" width="100%" src="https://youtube.com/embed/L_jWHffIx5E?autoplay=1&controls=0&showinfo=0&autohide=1"></iframe>
+<div id="accueil">
+    <div class="video">
+        <div>
+            <h1>Lylart</h1>
+            <a href="gen/"><div class="button">Générer</div></a>
+        </div>
+        <video autoplay loop> <!-- autoplay loop controls -->
+            <source src="video/EEEAAAOOO.mp4" type="video/mp4">
+        </video>
     </div>
 </div>
 
