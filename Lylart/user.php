418734af73d43a2ae1328009608f16935bcaf2ad
Lygobate <63309083+Lygobate@users.noreply.github.com>
2020-12-15 01:35:58 +0100

    Avancement user.php
    Design quasi terminé. ajouter un input pour le mdp et c'est OK niveau design.

--- Lylart/user.php
+++ Lylart/user.php
@@ -14,23 +14,34 @@
       </div>
     </div>
 
-    <div class="info-box">
-      <div class="label">Nom d'utilisateur</div>
-      <input type="text" class="data"></input>
-      <input type="button" value="Changer pseudo"></input>
+    <div class="info-box info-box1 top">
+      <div class="label-form label-form-user">Nom d'utilisateur</div>
+      <div class="data-form data-form-user">
+        <input type="text" class="data-form data-form-user-input"></input>
+        <span>█</span>
+      </div>
+      <input type="button" class="button-form button-form-user" value="Changer pseudo"></input>
     </div>
-    <div class="info-box">
-      <div class="label">Email</div>
-      <input type="text" class="data"></input>
-      <input type="button" value="Changer d'Email"></input>
+    <div class="info-box info-box1">
+      <div class="label-form label-form-user">Email</div>
+      <div class="data-form data-form-user">
+        <input type="text" class="data-form data-form-user-input" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"></input>
+        <span>█</span>
+      </div>
+      <input type="button" class="button-form button-form-user" value="Changer d'Email"></input>
     </div>
-    <div class="info-box">
-      <div class="label">Mot de passe</div>
-      <input type="text" class="data"></input>
-      <input type="button" value="Changer mdp"></input>
+
+    <div class="info-box info-box-mdp">
+      <div class="label-form label-form-user">Mot de passe</div>
+      <div class="data-form data-form-user">
+        <input type="text" class="data-form data-form-user-input"></input>
+        <span>█</span>
+      </div>
+      <input type="button" class="button-form button-form-user" value="Changer mdp"></input>
     </div>
+
     <div class="osef-tous">
-      voici les trucs que personne ne lira, mais on vous recommande de le lire au cas où...
+      <p>voici les trucs que personne ne lira, mais on vous recommande de le lire au cas où...</p>
     </div>
   </div>
 </main>
