618ec1fe2652c8f78f0148a533dadadcf1d05ca7
Lygobate <63309083+Lygobate@users.noreply.github.com>
2020-12-16 01:28:06 +0100

    ca marche putain

--- Lylart/requetes/new_user_and_connection.php
+++ Lylart/requetes/new_user_and_connection.php
@@ -1,75 +1,99 @@
 <?php
 require 'config.php';
 
+$checkToken=$pass->prepare("SELECT token FROM utilisateur WHERE token = :token");
 $checkUserName=$pass->prepare("SELECT pseudo FROM utilisateur WHERE pseudo = :user");
 $checkMail=$pass->prepare("SELECT mail FROM utilisateur WHERE mail = :mail");
-$newUser=$pass->prepare("INSERT INTO utilisateur VALUES(1, :mail, :user, :password, 0, date('Y/m/d H:i:s'), :pays, 0,0)");
+$newUser=$pass->prepare("INSERT INTO utilisateur VALUES(:token, :mail, :user, :password, 0, :datecrea, :pays, 0,0)");
                                                       //token,mail,pseudo,mdp,verifie,date_inscription,pays,nb_strike,role
-$connectionTry=$pass->prepare("SELECT mdp FROM utilisateur WHERE pseudo = :pseudo");
+$connectionTry=$pass->prepare("SELECT mdp,token FROM utilisateur WHERE pseudo = :pseudo");
 
 
+function createToken(){
+  $token="";
+  $aleat = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+  $count=strlen($aleat);
+  for ($i = 0; $i < 8; $i++) {
+    $n = rand(0, $count-1);
+    $token .= $aleat[$n];
+  }
+  return $token;
+}
+$token= createToken();
 
-  if (isset($_POST["from"])) {
-    switch ($_POST["from"]) {
-      case 'creer':
-        $checkUserName->execute([":user"=>$_POST["login"]]);
-        $similarUserName=$checkUserName->fetch(PDO::FETCH_ASSOC);
-        $checkMail->execute([":mail"=>$_POST["mail"]]);
-        $similarMail=$checkMail->fetch(PDO::FETCH_ASSOC);
+if (isset($_POST["from"])) {
+  switch ($_POST["from"]) {
+    case 'creer':
 
-        if (empty($similarMail)) {
-          $doubleMail= "false";
+      $checkUserName->execute([":user"=>$_POST["login"]]);
+      $similarUserName=$checkUserName->fetchAll(PDO::FETCH_ASSOC);
+      $checkMail->execute([":mail"=>$_POST["mail"]]);
+      $similarMail=$checkMail->fetchAll(PDO::FETCH_ASSOC);
+      $checkToken->execute([":token"=>$token]);
+      $similarToken=$checkToken->fetchAll(PDO::FETCH_ASSOC);
+
+      while (!empty($similarToken)) {
+        //echo "non mais oh, copieur de l'improbable";
+        $token = createToken();
+        $checkToken->execute([":token"=>$token]);
+        $similarToken=$checkToken->fetchAll(PDO::FETCH_ASSOC);
+      }
+
+      if (empty($similarMail)) {
+        $doubleMail= false;
+      }
+      else {
+        $doubleMail = true;
+
+      }
+
+      if (empty($similarUserName)) {
+        $doubleUserName= false;
+      }
+      else {
+        $doubleUserName = true;
+      }
+
+      if ($doubleMail == true && $doubleUserName == true) {
+        $registed = "mail_and_userName_error";
+      }
+      else if ($doubleMail == true && $doubleUserName == false) {
+        $registed = "mail_error";
+      }
+      else if ($doubleMail == false && $doubleUserName == true) {
+        $registed = "userName_error";
+      }
+      else {
+        $registed = "registed";
+        $newUser->execute([":token"=>$token, ":mail"=>$_POST["mail"], ":user"=>$_POST["login"], ":password"=>password_hash($_POST["password"], PASSWORD_BCRYPT), ":datecrea"=>date('Y-m-d H:i:s'), ":pays"=>$_POST["pays"]]);
+
+        session_start();
+        $_SESSION['token'] = $token;
+      }
+      echo($registed);
+      break;
+
+    case 'connection':
+      $connectionTry->execute([":pseudo"=>$_POST["login"]]);
+      $mdp = $connectionTry->fetchAll(PDO::FETCH_ASSOC);
+      print_r($mdp);
+      if (empty($mdp)) {
+        $connection = "false";
+      }
+      else {
+        if (password_verify($_POST["password"], $mdp[0]["mdp"])){
+          session_start();
+          $_SESSION['token'] = $mdp[0]["token"];
+          setcookie("token_ma_gueule_!", $mdp[0]["token"]);
+          $connection = "true";
         }
         else {
-          $doubleMail = "true";
-        }
-
-        if (empty($similarUserName)) {
-          $doubleUserName= "false";
-        }
-        else {
-          $doubleUserName = "true";
-        }
-
-        if ($doubleMail == true && $doubleUserName == true) {
-          $registed = "mail_and_userName_error";
-        }
-        else if ($doubleMail == true && $doubleUserName == false) {
-          $registed = "mail_error";
-        }
-        else if ($doubleMail == false && $doubleUserName == true) {
-          $registed = "userName_error";
-        }
-        else {
-          $registed = "registed";
-          $newUser->execute([":mail"=>$_POST["mail"], ":user"=>$_POST["login"], ":password"=>$_POST["password"], ":pays"=>$_POST["pays"]]);
-
-          //session_start();
-          //$_SESSION['token'] = $token;
-        }
-        echo($registed);
-        break;
-
-      case 'connexion':
-        $connectionTry=execute([":pseudo"=>$_POST["login"]]);
-        $mdp = $connectionTry->fetch(PDO::FETCH_ASSOC);
-
-        if (empty($mdp)) {
           $connection = "false";
         }
-        else {
-          if ($mdp["mdp"] == $_POST["mdp"]) {
-            //session_start();
-            //$_SESSION['token'] = $token;
-            $connection = "true";
-          }
-          else {
-            $connection = "false";
-          }
-        }
-        echo($connection);
-        break;
-    }
+      }
+      echo($connection);
+      break;
   }
+}
 
 ?>
